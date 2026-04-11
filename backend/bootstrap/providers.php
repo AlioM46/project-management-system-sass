3d8f1d4aaa2fe641f279576bfe80bc60f6e1adfb
AlioM46 <aliomaralsloom@gmail.com>
2026-04-11 22:34:30 +0300

    chore(projects): register projects module service provider

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Modules\Auth\AuthServiceProvider::class,
+    App\Modules\Projects\ProjectsServiceProvider::class,
     App\Modules\RolesPermissions\RolesPermissionsServiceProvider::class,
     App\Modules\Workspace\WorkspaceServiceProvider::class,
 ];
