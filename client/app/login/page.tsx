eb2aa4c4ada97ad1abcb835e9cdf347dc2ffcf28
AlioM46 <aliomaralsloom@gmail.com>
2026-05-14 00:42:37 +0300

    refactor: clean up login page wrapper

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,15 +1,8 @@
 // This will be your Login page at 'http://localhost:3000/login'
+import Login from "@/features/auth/pages/Login";
 
 export default function LoginPage() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50">
-            <div className="text-center">
-                <h1 className="text-2xl font-bold">Login Page Placeholder</h1>
-                <p className="mt-2 text-gray-600">You're going to build this part!</p>
-                <a href="/register" className="mt-4 inline-block text-indigo-600 font-bold">
-                    Go back to Register
-                </a>
-            </div>
-        </div>
-    );
+        <Login />
+    )
 }
