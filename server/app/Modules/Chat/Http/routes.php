9c65cfc2bc7bf7fd9217435501d2b8d1c44b519e
AlioM46 <aliomaralsloom@gmail.com>
2026-07-28 12:32:43 +0300

    feat(chat-routes): register chat module routes and service provider

---
+++ server/app/Modules/Chat/Http/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Modules\Chat\Http\Controllers\ConversationController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('conversations')
+    ->middleware(['auth:api', 'workspace.context'])
+    ->group(function () {
+        Route::get('/', [ConversationController::class, 'index']);
+        Route::post('/', [ConversationController::class, 'store']);
+        Route::get('/{id}/messages', [ConversationController::class, 'getMessages'])->whereNumber('id');
+        Route::post('/{id}/messages', [ConversationController::class, 'sendMessage'])->whereNumber('id');
+    });
