9d11cdca08ae0efc2f32b06307c03e917c88a6bf
AlioM46 <aliomaralsloom@gmail.com>
2026-08-10 11:48:53 +0300

    refactor(chat): split ConversationController into ChatMessageController and MessageReactionController

--- server/app/Modules/Chat/Http/routes.php
+++ server/app/Modules/Chat/Http/routes.php
@@ -1,33 +1,35 @@
 <?php
 
 use App\Modules\Chat\Http\Controllers\BlockController;
+use App\Modules\Chat\Http\Controllers\ChatMessageController;
 use App\Modules\Chat\Http\Controllers\ConversationController;
+use App\Modules\Chat\Http\Controllers\MessageReactionController;
 use App\Modules\Chat\Http\Controllers\ParticipantsController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('conversations')
     ->middleware(['auth:api', 'workspace.context'])
     ->group(function () {
+        // Conversation Management Routes
         Route::get('/', [ConversationController::class, 'index']);
         Route::post('/', [ConversationController::class, 'store']);
-        Route::get('/{id}/messages', [ConversationController::class, 'getMessages'])->whereNumber('id');
-        Route::post('/{id}/messages', [ConversationController::class, 'sendMessage'])->whereNumber('id');
-        Route::delete('/{id}/messages/{messageId}/deleteForMe', [ConversationController::class, 'deleteForMe'])->whereNumber('id')->whereNumber('messageId');
-
-        Route::delete('/{id}/messages/{messageId}/delete', [ConversationController::class, 'deleteForAll'])->whereNumber('id')->whereNumber('messageId');
-
-        Route::put('/{id}/messages/{messageId}/updateForMe', [ConversationController::class, 'update'])->whereNumber('id')->whereNumber('messageId');
-
-        Route::post('/{id}/messages/{messageId}/reactions', [ConversationController::class, 'toggleReaction'])->whereNumber('id')->whereNumber('messageId');
-        Route::post('/{id}/messages/{messageId}/star', [ConversationController::class, 'toggleStarMessage'])->whereNumber('id')->whereNumber('messageId');
-
-        Route::get('/{id}/messages/search', [ConversationController::class, 'searchMessages'])->whereNumber('id');
-        Route::get('/{id}/starred', [ConversationController::class, 'getStarredMessages'])->whereNumber('id');
-
         Route::get('/{id}/info', [ConversationController::class, 'sidebarInfo'])->whereNumber('id');
         Route::put('/{id}', [ConversationController::class, 'updateDetails'])->whereNumber('id');
 
-        // Conversation Actions Routes
+        // Message Operations Routes
+        Route::get('/{id}/messages', [ChatMessageController::class, 'getMessages'])->whereNumber('id');
+        Route::post('/{id}/messages', [ChatMessageController::class, 'sendMessage'])->whereNumber('id');
+        Route::get('/{id}/messages/search', [ChatMessageController::class, 'searchMessages'])->whereNumber('id');
+        Route::put('/{id}/messages/{messageId}/updateForMe', [ChatMessageController::class, 'update'])->whereNumber('id')->whereNumber('messageId');
+        Route::delete('/{id}/messages/{messageId}/deleteForMe', [ChatMessageController::class, 'deleteForMe'])->whereNumber('id')->whereNumber('messageId');
+        Route::delete('/{id}/messages/{messageId}/delete', [ChatMessageController::class, 'deleteForAll'])->whereNumber('id')->whereNumber('messageId');
+
+        // Message Reactions & Starred Routes
+        Route::post('/{id}/messages/{messageId}/reactions', [MessageReactionController::class, 'toggleReaction'])->whereNumber('id')->whereNumber('messageId');
+        Route::post('/{id}/messages/{messageId}/star', [MessageReactionController::class, 'toggleStarMessage'])->whereNumber('id')->whereNumber('messageId');
+        Route::get('/{id}/starred', [MessageReactionController::class, 'getStarredMessages'])->whereNumber('id');
+
+        // Conversation Action Routes
         Route::post('/{id}/clear', [ParticipantsController::class, 'ClearConversation'])->whereNumber('id');
         Route::delete('/{id}', [ParticipantsController::class, 'DeleteConversation'])->whereNumber('id');
         Route::post('/{id}/mute', [ParticipantsController::class, 'ToggleMute'])->whereNumber('id');
